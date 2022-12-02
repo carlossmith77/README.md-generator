236582e85cd4a63ea2ee31e77ffbc69d0edd16b4
carlossmith77 <carlosking12377Gmail.com>
2022-12-01 23:31:58 -0600

    changed the inputs started working on the function

--- Develop/index.js
+++ Develop/index.js
@@ -3,28 +3,60 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown')
 // TODO: Create an array of questions for user input
-    
-inquirer.prompt([
+const prerequisites = [
+  //inquirer.prerequisite([
       {
+      
         type: 'input',
         message: 'what is the name of your product.',
-        name: 'Title',
+        name: 'Name',
       },
       {
         type: 'input',
-        message: 'Please give a description of your project. including how to install it, the practical use case for the product, please provide the license that was used, please also give you email and git hub username.',
+        message: 'Please give a description of your product.',
         name: 'Description',
       },
+      {
+        type: 'input',
+        message: 'Please explain how to instal the product. ',
+        name: 'Installation',
+      },
+      {
+        type: 'input',
+        message: 'what is the real world use cae for this product?',
+        name: 'Use-case',
+      },
+      {
+        type: 'input',
+        message: 'what license are you using?',
+        name: 'License',
+      },
+      {
+        type: 'input',
+        message: 'Please enter your github username.',
+        name: 'Github',
+      },
+      {
+        type: 'input',
+        message: 'Please enter your email.',
+        name: 'Email',
+      },
     
-    
-    ])
-
+    ]
+    //)]
+    // .prompt(prerequisites)
+    // .then(function(data))
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 //Description
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+inquirer.prompt(prerequisites)
+.then((event)=>console.log(event))
+
+
+}
 
 // Function call to initialize app
-init(run);
+init();
