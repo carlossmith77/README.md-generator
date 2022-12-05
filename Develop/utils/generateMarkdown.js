9bd1c43e450e4ece746cc111c19bcd25a60c0a06
carlossmith77 <carlosking12377Gmail.com>
2022-12-04 20:37:26 -0600

    tweaked markdown

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -8,18 +8,24 @@ function renderLicenseBadge(license) { if (!license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(Licenses) {}
 if (license === 'MIT') {
   return `https://lbesson.mit-license.org/`
-}
+} 
+
+if (license === 'GPL') {
+  return `http://perso.crans.org/besson/LICENSE.html`
+} 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {}
-if (license === "MIT") {
-  return `Your license is supported `;
-} else {
+if (!license) {
+  return ``;
+}   else {
   return `your license is not supported by this product.`
 }
+
+
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.prerequisites}
@@ -40,7 +46,7 @@ function generateMarkdown(data) {
   
   ## Use-case
 
-  ${data.use-case}
+  ${data.use-cases}
 
   ## Licenses
 
